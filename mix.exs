eb9d2157266bf40e969bdc80f19c856100099ad4
Xabi <xabi.aycaguer@gmail.com>
2022-02-15 08:25:17 +0100

    last commit

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Chat.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:mnemonic_slugs, "~> 0.0.3"},
       {:phoenix, "~> 1.6.6"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
@@ -45,7 +46,8 @@ defmodule Chat.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:uuid, "~> 1.1.8"}
     ]
   end
 
